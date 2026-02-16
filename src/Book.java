8d9de655cd951ccb7a770a787a0a7d31059f4fdf
Melissa Gomez <melissagomez3024@gmail.com>
2026-02-16 14:26:29 +0100

    feat: add constructor and getters/setters

--- src/Book.java
+++ src/Book.java
@@ -4,4 +4,52 @@ public class Book {
     private String isbn;
     private String writer;
     private double price;
+
+    public Book(String title, String description, String writer, String isbn, double price) {
+        this.title = title;
+        this.description = description;
+        this.writer = writer;
+        this.isbn = isbn;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
